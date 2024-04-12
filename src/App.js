336067e8fff5b0561aa5b5bf4ad59a3ef7adeca4
sunyingjie <jusdy_xiao7@163.com>
2024-04-12 13:55:44 +0800

    bootstrap布局示例

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="App container-fluid">
+      <div className="row">
+        <div className="col-3 bg-danger left-panel">
+          <h1>this is the left</h1>
+        </div>
+        <div className="col-9 bg-primary right-panel">
+          <h1>this is the left</h1>
+        </div>
+      </div>
     </div>
   );
 }
