cb408d3c9648a29110152d3187de5e65d17b71ec
sunyingjie <jusdy_xiao7@163.com>
2024-04-13 00:04:51 +0800

    FileList组件

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 // 引入bootstrap样式库文件
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FileSearch from "./components/FileSearch";
+import FileList from "./components/FileList";
+import defaultFiles from "./utils/defaultFiles";
 
 function App() {
   return (
@@ -12,6 +14,12 @@ function App() {
           <FileSearch
           title='我的云文档'
           onFileSearch={(value) => {console.log(value)}}
+          />
+          <FileList
+            files={defaultFiles}
+            onSaveEdit={(id, newValue) => {console.log(id); console.log(newValue);}}
+            onFileClick={(id) => console.log('clicking', id)}
+            onFileDelete={(id) => console.log('deleting', id)}
           />
         </div>
         <div className="col-9 bg-primary right-panel">
---
+++ src/components/FileList.js
@@ -0,0 +1,130 @@
+/**
+ * @作者 Conor Sun
+ * @开源仓库 $ https://github.com/jusdyxiao7
+ * @创建时间 2024/4/12 星期五 下午 13:59
+ */
+import React, {useState, useEffect, useRef} from "react";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faEdit, faTrash, faTimes} from '@fortawesome/free-solid-svg-icons'
+import {faMarkdown} from '@fortawesome/free-brands-svg-icons'
+import PropTypes from 'prop-types'
+
+const FileList = ({files, onFileClick, onSaveEdit, onFileDelete}) => {
+    const [editStatus, setEditStatus] = useState(false)
+    const [value, setValue] = useState('');
+
+    const closeSearch = (e) => {
+        e.preventDefault()
+        setEditStatus(false)
+        setValue('')
+    }
+
+    // 增加键盘回车和Esc事件
+    useEffect(() => {
+        const handleInputEvent = (event) => {
+            const {keyCode} = event
+            // 回车 13
+            // Ese 27
+            if (keyCode === 13 && editStatus) {
+                const editItem = files.find(file => file.id === editStatus)
+                onSaveEdit(editItem.id, value)
+                setEditStatus(false)
+                setValue('')
+            } else if (keyCode === 27 && editStatus) {
+                closeSearch(event)
+            }
+        }
+        document.addEventListener('keyup', handleInputEvent)
+        return () => {
+            document.removeEventListener('keyup', handleInputEvent)
+        }
+    })
+
+
+    return (
+        <ul className="list-group list-group-flush file-list">
+            {
+                files.map(file => (
+                    <li
+                        className="list-group-item bg-light d-flex align-items-center row"
+                        key={file.id}
+                    >
+                        { editStatus !== file.id && (
+                            <>
+                                <span className="col-2">
+                                <FontAwesomeIcon
+                                    size="lg"
+                                    icon={faMarkdown}
+                                />
+                                </span>
+                                <span
+                                    className="col-8 c-link"
+                                    onClick={() => onFileClick(file.id)}
+                                >{file.title}
+                                </span>
+                                <button
+                                    type="button"
+                                    className="icon-button col-1"
+                                    onClick={() => {
+                                        setEditStatus(file.id);
+                                        setValue(file.title)
+                                    }}>
+                                    <FontAwesomeIcon
+                                        title="编辑"
+                                        size="lg"
+                                        icon={faEdit}
+                                    />
+                                </button>
+                                <button
+                                    type="button"
+                                    className="icon-button col-1"
+                                    onClick={() => onFileDelete(file.id)}>
+                                    <FontAwesomeIcon
+                                        title="删除"
+                                        size="lg"
+                                        icon={faTrash}
+                                    />
+                                </button>
+                            </>
+                        )}
+                        {
+                            editStatus === file.id && (
+                                <>
+                                    <div className="col-10">
+                                        <input
+                                            className="form-control"
+                                            value={value}
+                                            onChange={(e) => {
+                                                setValue(e.target.value)
+                                            }}
+                                        />
+                                    </div>
+
+                                    <button
+                                        type="button"
+                                        className="icon-button col-2"
+                                        onClick={closeSearch}>
+                                        <FontAwesomeIcon
+                                            title="关闭"
+                                            size="lg"
+                                            icon={faTimes}
+                                        />
+                                    </button>
+                                </>
+                            )
+                        }
+                    </li>
+                ))
+            }
+
+        </ul>
+    )
+}
+
+FileList.protoTypes = {
+    files: PropTypes.array,
+    onFileClick: PropTypes.func,
+    onSaveEdit: PropTypes.func,
+    onFileDelete: PropTypes.func,
+}
+export default FileList;
